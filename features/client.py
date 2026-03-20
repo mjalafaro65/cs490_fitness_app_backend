1b59ad6155b2b0b8d495474e82a90200c0fea19e
Mjacm15 <alfamar2005@gmail.com>
2026-03-19 20:34:29 -0400

    set up: survey schema and class

--- features/client.py
+++ features/client.py
@@ -3,12 +3,13 @@ from flask_smorest import Blueprint
 from models.daily_wellness import DailyWellness
 from db import db
 from datetime import date
+from schemas.client_schema import DailySurveySchema
 
 #for connecting to app
 client_blp=Blueprint("Client", __name__, url_prefix="/client", description="Client Operations")
 
-@client_blp.route("/survey")
-class Survey(MethodView):
-    # @client_blp.response(201, SurveySchema)
+@client_blp.route("/daily-survey")
+class DailySurvey(MethodView):
+    @client_blp.response(201, DailySurveySchema)
     def post(self):
-        return 201
+        
