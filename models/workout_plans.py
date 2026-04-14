0dfd3aad35b1c016d5e4fd5a707cd088de7b74d6
AntimatterSushi <49251550+AntimatterSushi@users.noreply.github.com>
2026-04-13 20:28:04 -0400

    Implemented feature

--- models/workout_plans.py
+++ models/workout_plans.py
@@ -10,6 +10,8 @@ class WorkoutPlans(db.Model):
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text)
     is_public= db.Column(db.Boolean)
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+    deleted_at = db.Column(db.DateTime, nullable=True)
     copied_from_plan_id = db.Column(db.Integer, db.ForeignKey('workout_plans.plan_id'), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
